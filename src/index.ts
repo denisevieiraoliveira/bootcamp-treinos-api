57d8769b7c6f0b5ac32c61e8ae09d429afcf4b01
denise.vieira <denise.vieira@ufj.edu.br>
2026-02-28 15:57:51 -0300

    chore: add scalar docs setup

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import fastifyCors from "@fastify/cors";
 import fastifySwagger from "@fastify/swagger";
-import fastifySwaggerUI from "@fastify/swagger-ui";
+import fastifyApiReference from '@scalar/fastify-api-reference';
 import "dotenv/config";
 import Fastify from "fastify";
 import {
@@ -36,13 +36,27 @@ await app.register(fastifySwagger, {
   transform: jsonSchemaTransform,
 });
 
-await app.register(fastifySwaggerUI, {
-  routePrefix: "/docs",
-});
-
 await app.register(fastifyCors, {
   origin: ["http://localhost:3000"],
   credentials: true,
+});
+
+await app.register(fastifyApiReference, {
+  routePrefix: "/docs",
+  configuration: {
+    sources: [
+      {
+        title: "Bootcamp Treinos API",
+        slug: "bootcamp-treinos-api",
+        url: "/swagger.json",
+      },
+      {
+        title: "Auth API",
+        slug: "auth-api",
+        url: "/api/auth/open-api/generate-schema",
+      },
+    ],
+  },
 });
 
 app.withTypeProvider<ZodTypeProvider>().route({
